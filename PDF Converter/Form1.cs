448dc0c8c8d7fe8ce355d20e1a11e0741ae2daf7
tjones14 <tazarian14@gmail.com>
2021-10-05 20:23:20 -0400

    Add project files.

---
+++ PDF Converter/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using convert = PDF_Converter.Utilities.Converters;
+
+namespace PDF_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+            var file = @"C:\Users\tazar\Desktop\Paste\20130922_141238.jpg";
+
+            // test for file type 
+
+
+            // if document is an image file
+            convert.imageConvert(file);
+        }
+    }
+}
---
+++ PDF Converter/Utilities/Converters.cs
@@ -0,0 +1,54 @@
+﻿using Spire.Pdf;
+using Spire.Pdf.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDF_Converter.Utilities
+{
+    public static class Converters
+    {
+
+        // public static interface GetConverter(string filename)
+        // Need to create an interface that will automatically call the method for the correct converter based
+        // on file type. 
+
+
+
+        public static PdfDocument ImageConverter(string fileName)
+        {
+
+            PdfDocument doc = new PdfDocument();
+            PdfSection section = doc.Sections.Add();
+            PdfPageBase page = doc.Pages.Add();
+
+            PdfImage image = PdfImage.FromFile(fileName);
+
+            float widthFitRate = image.PhysicalDimension.Width / page.Canvas.ClientSize.Width;
+            float heightFitRate = image.PhysicalDimension.Height / page.Canvas.ClientSize.Height;
+            float fitRate = Math.Max(widthFitRate, heightFitRate);
+            float fitWidth = image.PhysicalDimension.Width / fitRate;
+            float fitHeight = image.PhysicalDimension.Height / fitRate;
+            page.Canvas.DrawImage(image, 30, 30, fitWidth, fitHeight);
+
+            return doc;
+
+        }
+
+        public static PdfDocument WordConverter(string fileName)
+        {
+            PdfDocument doc = new PdfDocument();
+
+            var splitfile = fileName.Split('.');
+            var extension = splitfile[1].ToUpper();
+
+            doc.LoadFromFile(fileName);
+
+
+            return doc;
+        }
+
+    }
+}
